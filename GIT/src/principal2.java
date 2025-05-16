c9cf7341afabca656dae52d054291fb16a98db43
R1ul09 <128465165+R1ul09@users.noreply.github.com>
2025-05-16 08:55:24 +0200

    version 1.4

--- GIT/src/principal2.java
+++ GIT/src/principal2.java
@@ -7,6 +7,7 @@ public class principal2 {
 		System.out.println("version 1.1");
 		System.out.println("version 1.2");
 		System.out.println("version 1.3");
+		System.out.println("version 1.4");
 	}
 
 }
