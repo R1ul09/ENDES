8bc9b880663b54a691333a35a55f30294fb50eb6
R1ul09 <128465165+R1ul09@users.noreply.github.com>
2025-05-16 09:06:09 +0200

    Va

--- GIT/src/principal3.java
+++ GIT/src/principal3.java
@@ -4,6 +4,7 @@ public class principal3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Version 1");
+		System.out.println("Version 2");
 	}
 
 }
